aaa8cc62e341327ef847dec29d1daeeb98066884
Eg0r0k <egokakill@gmail.com>
2025-12-12 22:39:50 +0300

    refactor: add new variants for skeleton

--- src/components/ui/skeleton/Skeleton.vue
+++ src/components/ui/skeleton/Skeleton.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from "vue";
+import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
+
+const skeletonVariants = cva(
+  "animate-pulse ",
+  {
+    variants: {
+      variant: {
+        default: "bg-muted/80 rounded-md",
+        primary: "bg-primary/10 rounded-md",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+type SkeletonVariants = VariantProps<typeof skeletonVariants>;
 
 interface SkeletonProps {
   class?: HTMLAttributes["class"];
+  variant?: SkeletonVariants["variant"];
 }
 
 const props = defineProps<SkeletonProps>();
@@ -12,6 +31,6 @@ const props = defineProps<SkeletonProps>();
 <template>
   <div
     data-slot="skeleton"
-    :class="cn('animate-pulse rounded-md bg-primary/10', props.class)"
+    :class="cn(skeletonVariants({variant}), props.class)"
   />
 </template>
